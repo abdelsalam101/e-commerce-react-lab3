18c719a2022aba45ecbd66a12c38689a253c37af
abdelsalam101 <https://abdelsalam101.github.io/>
2025-05-19 16:36:28 +0300

    Add Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import Header from './components/Header';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css'
 import ProductList from './pages/ProductList';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import ProductDetail from './pages/ProductDetail';
-import Register from './pages/Register';
+// import Cart from './pages/Cart';
+// import NotFound from './pages/NotFound';
+// import ProductDetail from './pages/ProductDetail';
+// import Register from './pages/Register';
+import { useState, useEffect, lazy, Suspense } from 'react';
+import LanguageContext from './context/language';
 
+const Cart=lazy(() => (import('./pages/Cart')) )
+const NotFound=lazy(() => (import('./pages/NotFound')) )
+const ProductDetail=lazy(() => (import('./pages/ProductDetail')) )
+const Register=lazy(() => (import('./pages/Register')) )
 function App() {
+  const [language, setLanguage] = useState("en");
+    useEffect(() => {
+    document.body.dir = language === "ar" ? "rtl" : "ltr";
+  }, [language]);
   return (
-   <BrowserRouter>
-      <Header />
-      <div className="container my-5">
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/productDetail/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-             <Route path="/register" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-   </BrowserRouter>
+    <BrowserRouter>
+      <LanguageContext.Provider value={{ language, setLanguage }}>
+        <Header />
+        <div className="container my-5">
+          <Suspense fallback={<h2>Loading...</h2>}>
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/productDetail/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+           </Suspense>
+        </div>
+      </LanguageContext.Provider>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,52 @@
 import React from "react";
 import { Link } from "react-router";
 import { useSelector } from "react-redux";
+import LanguageContext from "../context/language";
+import { useContext } from "react";
 
 export default function Header() {
-const cartItems = useSelector((state) => state.cart) || [];
+  const cartItems = useSelector((state) => state.cart) || [];
   const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+  const { setLanguage } = useContext(LanguageContext);
 
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary p-2">
-      <div className="container-fluid ">
+      <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <span className="navbar-brand bg-danger rounded-3 p-2 text-white">
             Graham Store
           </span>
         </Link>
+
+        <div className="dropdown me-3">
+          <button
+            className="btn btn-secondary dropdown-toggle btn-sm"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Language
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <button
+                className="dropdown-item"
+                onClick={() => setLanguage("en")}
+              >
+                EN
+              </button>
+            </li>
+            <li>
+              <button
+                className="dropdown-item"
+                onClick={() => setLanguage("ar")}
+              >
+                AR
+              </button>
+            </li>
+          </ul>
+        </div>
+
         <div className="d-flex align-items-center gap-3">
           <Link to="/cart" className="position-relative text-dark fs-4">
             🛒
